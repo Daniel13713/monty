69114d531b017dddab3669650636726f393c081f
daniel <danydu1299@gmail.com>
2022-01-12 15:31:37 -0500

    opcode push and pall, add and show (solving betty)

--- main.c
+++ main.c
@@ -3,6 +3,25 @@
 int status = 0;
 
 /**
+ * _open - open a file
+ *
+ * @argv: file
+ * Return: open file
+ */
+FILE *_open(char *argv)
+{
+	FILE *file;
+
+	file = fopen(argv, "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv);
+		exit(EXIT_FAILURE);
+	}
+	return (file);
+}
+
+/**
  * main - main function
  *
  * @argc: number of arguments
@@ -12,45 +31,40 @@ int status = 0;
 
 int main(int argc, char *argv[])
 {
-    char *lines = NULL, *name = NULL, *value = NULL;
-    int i = 0;
-    FILE *file;
-    stack_t *stack = NULL;
-    size_t len = 0;
+	char *lines = NULL, *name = NULL, *value = NULL;
+	int i = 0;
+	FILE *file;
+	stack_t *stack = NULL;
+	size_t len = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = _open(argv[1]);
 
-    while (getline(&lines, &len, file) != -1)
-    {
-        name = strtok(lines, " \t\n");
-        if (!name || *name == '#')
-        {
-            i++;
-            continue;
-        }
-        value = strtok(NULL, " \t\n");
-        executer(&stack, name, value, i + 1);
-        if (status == EXIT_FAILURE)
-        {
-            free(lines);
-            free_stack(stack);
-            fclose(file);
-            exit(status);
-        }
-        i++;
-    }
-    free(lines);
-    fclose(file);
-    free_stack(stack);
-    return (0);
+	while (getline(&lines, &len, file) != -1)
+	{
+		name = strtok(lines, " \t\n");
+		if (!name || *name == '#')
+		{
+			i++;
+			continue;
+		}
+		value = strtok(NULL, " \t\n");
+		executer(&stack, name, value, i + 1);
+		if (status == EXIT_FAILURE)
+		{
+			free(lines);
+			free_stack(stack);
+			fclose(file);
+			exit(status);
+		}
+		i++;
+	}
+	free(lines);
+	fclose(file);
+	free_stack(stack);
+	return (0);
 }
